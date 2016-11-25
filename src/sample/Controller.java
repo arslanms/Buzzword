23725ffdcd1af66093e7ff5fcdfd0c57f94ee943
arslanms <arslanmsadiq@gmail.com>
2016-11-24 22:51:00 -0500

    Made a loop that will change whether a level has a lock or not

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,6 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
@@ -31,6 +32,12 @@ public class Controller {
     public TableColumn gameplayPointsColumn;
     public Stage loginStage;
     public Label levelSelectionModeTitle;
+    public Label levelSelectNodeLabel1, levelSelectNodeLabel2, levelSelectNodeLabel3, levelSelectNodeLabel4,
+            levelSelectNodeLabel5, levelSelectNodeLabel6, levelSelectNodeLabel7, levelSelectNodeLabel8;
+    public ImageView levelSelectNodeLock1, levelSelectNodeLock2, levelSelectNodeLock3, levelSelectNodeLock4,
+            levelSelectNodeLock5, levelSelectNodeLock6, levelSelectNodeLock7, levelSelectNodeLock8;
+    public Label[] levelSelectLabels;
+    public ImageView[] levelSelectLocks;
 
     public void addModes()  {
         modeComboBox.getItems().clear();
@@ -91,7 +98,7 @@ public class Controller {
 
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("levelselection.fxml"));
             Parent parent = fxmlLoader.load();
-            changeScene(parent, event);
+            changeScene(parent, event, fxmlLoader);
             setLevelSelection(fxmlLoader.getController());
 
         }
@@ -104,6 +111,60 @@ public class Controller {
 
         String modeTitle = modeComboBox.getValue();
         levelSelectionModeTitle.setText(modeTitle);
+
+        levelSelectNodeLabel1 = controller.levelSelectNodeLabel1;
+        levelSelectNodeLabel2 = controller.levelSelectNodeLabel2;
+        levelSelectNodeLabel3 = controller.levelSelectNodeLabel3;
+        levelSelectNodeLabel4 = controller.levelSelectNodeLabel4;
+        levelSelectNodeLabel5 = controller.levelSelectNodeLabel5;
+        levelSelectNodeLabel6 = controller.levelSelectNodeLabel6;
+        levelSelectNodeLabel7 = controller.levelSelectNodeLabel7;
+        levelSelectNodeLabel8 = controller.levelSelectNodeLabel8;
+
+        levelSelectLabels = new Label[8];
+
+        levelSelectLabels[0] = levelSelectNodeLabel1;
+        levelSelectLabels[1] = levelSelectNodeLabel2;
+        levelSelectLabels[2] = levelSelectNodeLabel3;
+        levelSelectLabels[3] = levelSelectNodeLabel4;
+        levelSelectLabels[4] = levelSelectNodeLabel5;
+        levelSelectLabels[5] = levelSelectNodeLabel6;
+        levelSelectLabels[6] = levelSelectNodeLabel7;
+        levelSelectLabels[7] = levelSelectNodeLabel8;
+
+        levelSelectNodeLock1 = controller.levelSelectNodeLock1;
+        levelSelectNodeLock2 = controller.levelSelectNodeLock2;
+        levelSelectNodeLock3 = controller.levelSelectNodeLock3;
+        levelSelectNodeLock4 = controller.levelSelectNodeLock4;
+        levelSelectNodeLock5 = controller.levelSelectNodeLock5;
+        levelSelectNodeLock6 = controller.levelSelectNodeLock6;
+        levelSelectNodeLock7 = controller.levelSelectNodeLock7;
+        levelSelectNodeLock8 = controller.levelSelectNodeLock8;
+
+        levelSelectLocks = new ImageView[8];
+
+        levelSelectLocks[0] = levelSelectNodeLock1;
+        levelSelectLocks[1] = levelSelectNodeLock2;
+        levelSelectLocks[2] = levelSelectNodeLock3;
+        levelSelectLocks[3] = levelSelectNodeLock4;
+        levelSelectLocks[4] = levelSelectNodeLock5;
+        levelSelectLocks[5] = levelSelectNodeLock6;
+        levelSelectLocks[6] = levelSelectNodeLock7;
+        levelSelectLocks[7] = levelSelectNodeLock8;
+
+        for (int i = 0; i < 1; i++) {
+            levelSelectLabels[i].setText(Integer.toString(i+1));
+            levelSelectLocks[i].setVisible(false);
+        }
+
+        for (int i = 1; i < 8; i++) {
+            levelSelectLabels[i].setText("");
+            levelSelectLocks[i].setVisible(true);
+        }
+
+
+
+
     }
 
     @FXML
@@ -111,7 +172,7 @@ public class Controller {
         levelSelectionModeTitle.setText("bruh");
     }
 
-    private void changeScene(Parent parent, ActionEvent event)  {
+    private void changeScene(Parent parent, ActionEvent event, FXMLLoader fxmlLoader)  {
         Scene scene = new Scene(parent, stageWidth, stageHeight);
         Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         appStage.setTitle("BuzzWord");
